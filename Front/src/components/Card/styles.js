953d1a11f00fc6cde131d83e5c975065d4b26977
Luan <j0hn04v@gmail.com>
2022-07-11 09:52:20 -0300

    Update styles.js

--- Front/src/components/Card/styles.js
+++ Front/src/components/Card/styles.js
@@ -12,14 +12,16 @@ export const SCard = styled.div`
 `;
 
 export const SSmallCard = styled.div`
-    max-width: 15vw;
-    width: 100%;
     max-height: 100%;
     background: ${({ theme }) => theme.bg};
     padding: ${v.lgSpacing};
     margin: ${v.mdSpacing} ${v.lgSpacing} ${v.lgSpacing} 0;
     border-radius: .5rem;
     box-shadow: 0 0 #0000, 0 0 #0000,0 1px 3px 0 rgba(0,0,0,.1), 0 1px 2px -1px rgba(0,0,0,.1);
+
+    @media(min-width: 1200px) {
+        max-width: 15vw;
+  }
 `;
 
 export const SCardHeader = styled.div`
